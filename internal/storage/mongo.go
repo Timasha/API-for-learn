82ebd5da5edfc02bd0431893a5a111d880277f5e
timofey <timashajz@yandex.ru>
2022-10-24 19:40:36 +0300

    fix errors handling in create/update/delete storage methods

--- internal/storage/mongo.go
+++ internal/storage/mongo.go
@@ -23,49 +23,71 @@ func (m *Mongo) Connect(ctx context.Context, ip, port string) error {
 	m.mongo, connErr = mongo.Connect(ctx, opt)
 	return connErr
 }
+
 func (m *Mongo) Close() {
 	m.mongo.Disconnect(context.Background())
 }
+
 func (m *Mongo) CreateUser(ctx context.Context, user User) (string, error) {
 	res, err := m.mongo.Database("app").Collection("users").InsertOne(ctx, user)
 
 	if _, ok := err.(mongo.WriteException); ok {
 		return "", ErrUserExist
 	}
+	if err != nil {
+		return "", err
+	}
 	if id, ok := res.InsertedID.(string); ok {
 		return id, err
 	}
+
 	return "", err
 }
 func (m *Mongo) ReadUser(ctx context.Context, login string) (User, error) {
 	res := m.mongo.Database("app").Collection("users").FindOne(ctx, bson.M{
 		"_id": login,
 	})
+
 	if res.Err() == mongo.ErrNoDocuments {
 		return User{}, ErrUserNotExist
 	}
+
 	if res.Err() != nil {
 		return User{}, res.Err()
 	}
+
 	var resultUser User
 	decErr := res.Decode(&resultUser)
+
 	return resultUser, decErr
 }
 func (m *Mongo) UpdateUser(ctx context.Context, login string, newUser User) error {
 	res, err := m.mongo.Database("app").Collection("users").ReplaceOne(ctx, bson.M{
 		"_id": login,
 	}, newUser)
+
+	if err != nil {
+		return err
+	}
+
 	if res.MatchedCount == 0 {
 		return ErrUserNotExist
 	}
-	return err
+
+	return nil
 }
 func (m *Mongo) DeleteUser(ctx context.Context, login string) error {
 	res, err := m.mongo.Database("app").Collection("users").DeleteOne(ctx, bson.M{
 		"_id": login,
 	})
+
+	if err != nil {
+		return err
+	}
+
 	if res.DeletedCount == 0 {
 		return ErrUserNotExist
 	}
-	return err
+
+	return nil
 }
